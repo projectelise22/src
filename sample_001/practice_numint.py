335bbcf8a13e5f0d54c41b89f79b1ba7480b9190
Dolly Palencia <dollypalencia.work@gmail.com>
2025-01-11 18:36:01 +0200

    Changes to be committed: 	new file:   sample_001/practice_functions.py 	modified:   sample_001/practice_numint.py

--- sample_001/practice_numint.py
+++ sample_001/practice_numint.py
@@ -20,9 +20,22 @@ res_1  = float(resp_1) ** float(resp_2)
 print(f"{resp_1} to the power of {resp_2} = {res_1}")
 
 # Number methods
-num_3 = [2.5, 3.5, 3.14159, 2.0, 4]
+num_3 = [2.5, 3.5, 3.14159, 2.0, 4, 0.887]
 print(round(num_3[0]))  # rounds down when even integer when last digit is 5
 print(round(num_3[1]))  # rounds up when odd integer when last digit is 5
 print(round(num_3[2], 3))  # rounds to nearest thirds
 for each in num_3:
     print(f"{each} is an integer?: {each.is_integer()}")
+
+# Exercise: 2 inputs and check the difference if an integer
+for i in range(3):
+    resp_3 = input("\n \nEnter number 1: ")
+    resp_4 = input("Enter number 2: ")
+    res_2 = round(float(resp_3) - 1,1)
+    print(f"The difference between {resp_3} and {resp_4} is {res_2}. Is it an integer? {res_2.is_integer()}!")
+
+print(f"\n{num_3[2]:.2f}")       # shows 2 decimals to the left
+print(f"{num_3[0]:.3f}")         # shows 3 decimals to the left
+print(f"{num_3[-1]:.0%}")        # shows percentage with 0 decimals to the left
+print(f"{num_3[-1]:.2%}")        # shows percentage with 2 decimals to the left
+print(f"{num_3[1] ** 15:,.2f}")  #shows number with comma as separator
