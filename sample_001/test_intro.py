7e01ce071c06a30dc83bb887bc5cd941f3f3fb57
Dolly Palencia <dollypalencia.work@gmail.com>
2025-02-08 11:38:35 +0200

    Change directory and filenames

--- sample_001/test_intro.py
+++
@@ -1,8 +0,0 @@
-# Sample usage of python
-# requests module should be imported before running this code
-
-import requests
-resp = requests.get("http://olympus.realpython.org")
-html = resp.text
-print(html[86:132])
-
